81bc940d34ec1ac82ae71a133ff16d385003216e
BDomogalla <BDDomogalla@gmail.com>
2019-12-23 18:03:34 -0800

    Moving Files

--- Instructions/StarterCode/Belly_Button_Biodiversity/app.js
+++
@@ -1,9 +0,0 @@
-
-d3.json("samples.json", function(error, data) {
-    console.log(data); // this is your data
-});
-
-
-//   C:\Users\BDDom\plotly-challenge\Instructions\StarterCode\Belly_Button_Biodiversity\samples.json
-//   Instructions\StarterCode\Belly_Button_Biodiversity\samples.json
-//   Instructions\StarterCode\Belly_Button_Biodiversity\static\js\app.js
