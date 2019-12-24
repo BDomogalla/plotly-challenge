d7cad02f2a36c8271db76107bba60bebdf9e508a
BDomogalla <BDDomogalla@gmail.com>
2019-12-23 17:54:51 -0800

    Finished Files

---
+++ Instructions/StarterCode/Belly_Button_Biodiversity/app.js
@@ -0,0 +1,9 @@
+
+d3.json("samples.json", function(error, data) {
+    console.log(data); // this is your data
+});
+
+
+//   C:\Users\BDDom\plotly-challenge\Instructions\StarterCode\Belly_Button_Biodiversity\samples.json
+//   Instructions\StarterCode\Belly_Button_Biodiversity\samples.json
+//   Instructions\StarterCode\Belly_Button_Biodiversity\static\js\app.js
